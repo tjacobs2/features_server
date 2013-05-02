66da5958430cce99abe219fd6d5d9cbdb1b3916e
Tim Jacobs <timjacobs2@gmail.com>
2013-05-01 23:55:27 -0400

    adding features server

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FeaturesServer::Application.config.secret_token = '9bba61a4356eefd781992191cb55d63af55a4963c890795648b9627f5cd80409279e515a1912aff63ba303952bd86a2ecc475617fdd82e4b8572cfd59f195a58'
